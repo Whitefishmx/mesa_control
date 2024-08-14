c87620bd96f1483fd65e74e3a088f89f96d4ffc6
DRAGKOZ <skdkuriel@hotmail.com>
2024-08-14 10:13:14 -0600

    config

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
 //    public string $baseURL = 'https://mesa-control.local';
-	public string $baseURL = 'https://mesa-control.solveshop.xyz/;
+	public string $baseURL = 'https://mesa-control.solveshop.xyz/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
